0b666dfbfc615fce0c4fb5a9978806adcede7687
Jeremy McCurdy <jeremy.mccurdy@redspace.com>
2022-11-07 10:33:54 -0400

    Update index.js

--- .github/actions/githubmergechecker/dist/index.js
+++ .github/actions/githubmergechecker/dist/index.js
@@ -89,7 +89,7 @@ async function run(octokit, { org,path, output }) {
   for(let i = 0; i< filteredResults.length; i++) 
   {
     rowData = filteredResults[i].name + "," + filteredResults[i].weeksSinceMainBranchMerge + "," + filteredResults[i].weeksSinceDevBranchMerge + "\n"
-    rows.push(stringify(data));
+    rows.push(data);
   }
   
 
